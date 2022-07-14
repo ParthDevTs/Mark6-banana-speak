826c80875c010ac896aeacc784ce393c3c437aa1
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2022-07-14 19:23:42 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var buttonTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#english-text");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function urlConstructor(text) {
+  return serverURL + "?text=" + text;
+}
+
+urlConstructor("parth");
+
+function doFetch(text) {
+  fetch(urlConstructor(text))
+    .then((response) => response.json())
+    .then((json) => (outputDiv.innerText = json.contents.translated))
+    .catch((error) => {
+      console.log("An Error Occured: " + error);
+      alert("Some error occured, please try after some time");
+    });
+}
 
 buttonTranslate.addEventListener("click", function clickEventHandler() {
-  console.log("clicked");
-  console.log(txtInput.value);
+  doFetch(txtInput.value);
 });
